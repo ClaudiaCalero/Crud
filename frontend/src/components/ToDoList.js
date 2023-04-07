f6a1a1ef8bba33b0785f1ad2b660c38625de5679
ClaudiaCalero <claudiaclrduro@gmail.com>
2023-04-07 22:22:52 +0200

    checkbox input in process

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -4,14 +4,26 @@ export default function ToDoList(props) {
     const data = props.data;
     function deleteTask(id) {
         axios.delete("http://localhost:8080/" + id)
-        console.log("delete" +id)
+        console.log("delete" + id)
+    }
+    function checkbox(id) {
+        axios.put("http://localhost:8080/1" + id)
+            .then(() => {
+                console.log('Task is succesfully completed');
+            })
+            .catch(error => {
+                console.error('Task is not completed', error);
+            });
     }
     return (
         <ul>
             {
                 data.map((toDo) => {
                     return (
-                        <li key={toDo.id}>{toDo.task}<button onClick={() => deleteTask(toDo.id)}>delete</button></li>
+                        <li key={toDo.id}>{toDo.task}
+                            <button onClick={() => deleteTask(toDo.id)}>delete</button>
+                            <input type="checkbox" onClick={() => checkbox(toDo.id)}></input>
+                        </li>
                     )
                 })
             }

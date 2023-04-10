bda280dd14c37699515cdc969a2dc4c34e634e00
ClaudiaCalero <claudiaclrduro@gmail.com>
2023-04-10 19:53:03 +0200

    Add edit functionality

---
+++ frontend/src/components/TextInput.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function TextInput(props) {
+    const text = props.text;
+    const setText = props.setText;
+    const placeholder = props.placeholder;
+    function onChangeHandler(event) {
+        setText(event.target.value);
+        console.log(event.target.value);
+    }
+    return (
+        <input value={text} type="text" placeholder={placeholder} onChange={(event) => onChangeHandler(event)}/>
+    )
+
+}
--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -1,34 +1,71 @@
 import axios from 'axios';
 import Checkbox from './Checkbox';
+import TextInput from './TextInput';
+import { useState } from 'react';
 
 export default function ToDoList(props) {
     const data = props.data;
+    const [selectedTodo, setSelectedTodo] = useState();
+    const [newToDoName, setNewToDoName] = useState("");
+    const [editToDoName, setEditToDoName] = useState("");
+
     function deleteTask(id) {
         axios.delete("http://localhost:8080/" + id)
-        console.log("delete" + id)
+        console.log('delete' + id)
     }
-    function createTask() {
-
+    function createToDo() {
+        const toDo = {
+            completed: false,
+            content: newToDoName
+        };
+        axios.post("http://localhost:8080/", toDo)
+            .then(() => {
+                console.log('To do successfully created');
+            })
+            .catch(error => {
+                console.error('To do unsuccessfully created', error);
+            });
+    }
+    function updateToDo() {
+        const editToDo = {
+            completed: selectedTodo.completed,
+            content: editToDoName
+        };
+        axios.put("http://localhost:8080/" + selectedTodo.id, editToDo)
+            .then(() => {
+                console.log('To do successfully updated');
+            })
+            .catch(error => {
+                console.error('To do unsuccessfully updated', error);
+            });
     }
 
     return (
         <div>
-            <section class="task-input">
-                <input type="text" placeholder="Add a new task" />
+            <section>
+                <TextInput text={newToDoName} setText={setNewToDoName} placeholder="Add a new task"/>
+                <button onClick={() => createToDo()}>+</button>
             </section>
             <ul>
                 {
                     data.map((toDo) => {
                         return (
-                            <li key={toDo.id}>
-                                <Checkbox toDo={toDo} />
-                                {toDo.task}
-                                <button onClick={() => deleteTask(toDo.id)}>delete</button>
-                            </li>
-                        )
-                    })
+                        <li key={toDo.id}>
+                            <Checkbox toDo={toDo} />
+                            {toDo.content}
+                            <button onClick={() => {
+                                setSelectedTodo(toDo)
+                                setEditToDoName(toDo.content)
+                            }}>Edit</button>
+                            <button onClick={() => deleteTask(toDo.id)}>Delete</button>
+                        </li>
+                    )})
                 }
             </ul>
+            <section>
+                <TextInput text={editToDoName} setText={setEditToDoName} placeholder="Select a task to edit"/>
+                <button onClick={() => updateToDo()}>Save</button>
+            </section>
         </div>
     )
 }

6359de03c5e077d51390957b2549a04240049903
ClaudiaCalero <claudiaclrduro@gmail.com>
2023-04-11 14:01:12 +0200

    Rewrote get functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,98 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import ToDoList from './components/ToDoList';
 import axios from 'axios';
+import Checkbox from './components/Checkbox';
+import TextInput from './components/TextInput';
+
 
 function App() {
   const [toDos, setToDos] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:8080/")
-      .then((response) => {
-        setToDos(response.data);
+  const [selectedTodo, setSelectedTodo] = useState();
+  const [newToDoName, setNewToDoName] = useState("");
+  const [editToDoName, setEditToDoName] = useState("");
+
+  function deleteTask(id) {
+    axios.delete("http://localhost:8080/" + id)
+    .then(() => {
+      console.log('delete' + id);
+      refreshToDos(); 
+    })
+    .catch(error => {
+      console.error('Task could not be deleted', error);
+    });
+}
+
+  function createToDo() {
+    const toDo = {
+      completed: false,
+      content: newToDoName
+    };
+    axios.post("http://localhost:8080/", toDo)
+      .then(() => {
+        console.log('To do successfully created');
+        refreshToDos();
       })
-  }, [])
+      .catch(error => {
+        console.error('To do unsuccessfully created', error);
+      });
+  }
+
+  function updateToDo() {
+    const editToDo = {
+      completed: selectedTodo.completed,
+      content: editToDoName
+    };
+    axios.put("http://localhost:8080/" + selectedTodo.id, editToDo)
+      .then(() => {
+        console.log('To do successfully updated');
+        refreshToDos();
+      })
+      .catch(error => {
+        console.error('To do unsuccessfully updated', error);
+      });
+  }
+
+  function refreshToDos() {
+    axios.get("http://localhost:8080/")
+    .then((response) => {
+      setToDos(response.data);
+    })
+  }
+
+  useEffect(() => {
+    refreshToDos();
+  }, []);
+
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <ToDoList
-        data={toDos}
-      />
-
+      <div>
+        <section>
+          <TextInput text={newToDoName} setText={setNewToDoName} placeholder="Add a new task" />
+          <button onClick={() => createToDo()}>+</button>
+        </section>
+        <ul>
+          {
+            toDos.map((toDo) => {
+              return (
+                <li key={toDo.id}>
+                  <Checkbox toDo={toDo} />
+                  {toDo.content}
+                  <button onClick={() => {
+                    setSelectedTodo(toDo)
+                    setEditToDoName(toDo.content)
+                  }}>Edit</button>
+                  <button onClick={() => deleteTask(toDo.id)}>Delete</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <section>
+          <TextInput text={editToDoName} setText={setEditToDoName} placeholder="Select a task to edit" />
+          <button onClick={() => updateToDo()}>Save</button>
+        </section>
+      </div>
     </div>
   );
 }
